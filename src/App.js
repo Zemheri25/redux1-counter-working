d74dbe2bb7094c2646093174109af81ddbbd43d6
Zemheri25 <omerzemheri14@gmail.com>
2022-04-15 01:51:57 +0300

    redux-1 counterworking added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './redux/store';
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
+  
+  
+  
+  
+  const increment = () => {
+    dispatch(actions.increment())
+  }
+
+  const decrement = () => {
+    dispatch(actions.decrement())
+  }
+
+  const addValue10 = () => {
+    dispatch(actions.addBy(30))
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h1>My number : {counter}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={addValue10}>Add by 10</button>
     </div>
   );
 }
